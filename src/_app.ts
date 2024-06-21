d80b334178577e65de2231019309ec5b970916d5
wobsoriano <sorianorobertc@gmail.com>
2024-06-21 08:15:52 -0700

    fix env vars

--- src/_app.ts
+++ src/_app.ts
@@ -3,6 +3,6 @@ import { clerkPlugin } from 'vue-clerk';
 
 export default (app: App) => {
   app.use(clerkPlugin, {
-    publishableKey: 'pk_test_Y2VydGFpbi1jb3lvdGUtNy5jbGVyay5hY2NvdW50cy5kZXYk',
+    publishableKey: import.meta.env.PUBLIC_CLERK_PUBLISHABLE_KEY,
   });
 };
